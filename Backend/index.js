958d1ac549a6766e5ad343147ee6b95a3b3e8d0a
Ayush Chouhan <ayush.chouhan0609@gmail.com>
2026-06-29 21:58:38 +0530

    Update CORS for deployment

--- Backend/index.js
+++ Backend/index.js
@@ -10,11 +10,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors(
-    {origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
-       credentials: true
-    }
-));
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
